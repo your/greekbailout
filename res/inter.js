37ab706d727ab41917c1b08b2ac8f0bff620c23e
segfault <sf@sharped.net>
2015-07-02 19:04:05 +0200

    Adding delay

--- res/inter.js
+++ res/inter.js
@@ -10,9 +10,8 @@ function inter() {
   $.getJSON( "res/data.json", function( data ) {
     var items = [];
     $.each( data, function( key, val ) {
-      $( '#' + key ).fadeOut( 1000 );
-      $( '#' + key ).html( val );
-      $( '#' + key ).fadeIn( 1000 );
+      $( '#' + key ).fadeOut( 1000 ).delay( 1000 ).fadeIn( 1000 );
+      $( '#' + key ).delay( 1000 ).html( val );
       $( "#loading" ).removeClass( "fa-spin" );
     });
   });
